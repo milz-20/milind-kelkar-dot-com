cdaae1ec520cd8631ef0eef3dfb68c93846ffd6c
Milind <mikemilu51@gmail.com>
2026-05-02 20:33:33 +0530

    Fix syntax error in import statement for Placeholder extension

--- my-notes-app/app/note/page.tsx
+++ my-notes-app/app/note/page.tsx
@@ -4,7 +4,7 @@ import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
-import Placeholder from '@tiptap/extension-placeholder',
+import Placeholder from '@tiptap/extension-placeholder'
 import Image from '@tiptap/extension-image'
 import Link from 'next/link'
 import {
